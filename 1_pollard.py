81cad8033206f3f96c605ada4aebfc5771959b54
Christopher Seaman <christopher.seaman@gmail.com>
2020-03-24 20:15:23 -0700

    Pollard's Rho works, need to interpret the result

---
+++ 1_pollard.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+
+# So the principle of Pollard's Rho is to find an overlapping cycle between the generator and the target
+# It was first explained to me as a pair of kangaroos hopping around the outback looking for eachother
+# Once one kangaroo finds the footprint of the other it can follow the trail to find it home
+
+from gmpy2 import mpz, powmod, random_state, mpz_random
+
+q       = mpz(1019)    # modulo of finite field
+q_order = mpz(1018)    # order of the field 
+g       = mpz(2)       # generator in Z_q
+k       = mpz(22)
+n       = powmod(g, k, q)
+
+print("b = g^k mod q = " + str(g) + "^" + str(k) + " mod " + str(q) + " = " + str(n))
+
+def iter(triple):
+    x = triple[0]
+    a = triple[1]
+    b = triple[2]
+    if   x % 3 == 0:
+        # print(0, x, a, b)
+        x = (x * x) % q
+        a = (a * 2) % q_order
+        b = (b * 2) % q_order
+        # print(0, x, a, b)
+    elif x % 3 == 1:
+        # print(1, x, a, b)
+        x = (x * g) % q
+        a = (a + 1) % q_order
+        # print(1, x, a, b)
+    elif x % 3 == 2:
+        # print(2, x, a, b)
+        x = (x * n) % q
+        b = (b + 1) % q_order
+        # print(2, x, a, b)
+    return [x, a, b]
+
+xab = [1, 0, 0]
+XAB = [1, 0, 0]
+i = mpz(0)
+
+# print(xab)
+# xab = iter(xab)
+# print(xab)
+# xab = iter(xab)
+# print(xab)
+
+print(str(i), str(xab[0]), str(xab[1]), str(xab[2]), str(XAB[0]), str(XAB[1]), str(XAB[2]))
+while i < q_order:
+    i += 1
+    xab = iter(xab)
+    XAB = iter(XAB)
+    XAB = iter(XAB)
+    print(str(i), str(xab[0]), str(xab[1]), str(xab[2]), str(XAB[0]), str(XAB[1]), str(XAB[2]))
+    if xab[0] == XAB[0]:
+        break
+
